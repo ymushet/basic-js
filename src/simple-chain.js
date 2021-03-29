809299fd27de97aa7b2469ed47903d061a0e75d2
Yehor Mushet <egor.mushet@gmail.com>
2021-03-29 22:44:11 +0200

    feate: transform-array and simple chain done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,44 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof value === "undefined") {
+      this.chain.push("( )");
+    } else {
+      this.chain.push("( " + String(value) + " )");
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (
+      typeof position === "number" &&
+      position <= this.chain.length - 1 &&
+      position > 0
+    ) {
+      this.chain.splice(position - 1, 1);
+    } else {
+      this.chain = [];
+      throw new CustomError("Error");
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let result = '';
+    if (this.getLength(this.chain) > 1) {
+      result = this.chain.join("~~");
+
+    } else {
+      result = this.chain.join("");
+    }
+    this.chain = [];
+    return result;
+  },
 };
 
 module.exports = chainMaker;
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  errorHandling(arr);
+  let arrayResult = arr.concat();
+  for (let i = 0; i < arrayResult.length; i++) {
+      discardNext(arrayResult, i);
+      discardPrev(arrayResult, i);
+      doubleNext(arrayResult, i);
+      doublePrev(arrayResult, i);
+  }
+  arrayResult = arrayResult.filter(item => item !== '--discard-next' && item !== '--double-prev' && item !== '--double-next' && item !== '--discard-prev');
+  return arrayResult;
 };
+
+function errorHandling(arr) {
+  if (!arr || Array.isArray(arr) !== true)
+      throw new CustomError('Error');
+}
+
+function discardNext(arrayResult, i) {
+  if (arrayResult[i] === '--discard-next') {
+      if (typeof arrayResult[i + 1] !== 'undefined')
+          arrayResult.splice(i + 1, 1);
+  }
+}
+
+function discardPrev(arrayResult, i) {
+  if (arrayResult[i] === '--discard-prev') {
+      if (typeof arrayResult[i - 1] !== 'undefined')
+          arrayResult.splice(i - 1, 1);
+  }
+}
+
+function doubleNext(arrayResult, i) {
+  if (arrayResult[i] === '--double-next') {
+      if (typeof arrayResult[i + 1] !== 'undefined')
+          arrayResult.splice(i, 1, arrayResult[i + 1]);
+  }
+}
+function doublePrev(arrayResult, i) {
+  if (arrayResult[i] === '--double-prev') {
+      if (typeof arrayResult[i - 1] !== 'undefined')
+          arrayResult.splice(i, 1, arrayResult[i - 1]);
+  }
+}
